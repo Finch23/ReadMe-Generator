6ea894b07e990732df84474185eb023073876cb9
Nicholas Finch <nicfinch223@gmail.com>
2020-03-06 11:45:57 -0500

    Final changes

--- index.js
+++ index.js
@@ -52,50 +52,51 @@ inquirer.prompt([
 
 ]).then(function(data) {
     axios
-    .get(`https://api.github.com/users/${username}`)
+    .get(`https://api.github.com/users/${data.username}`)
     .then(function(res) {
+        const getLicense = (license) => {
+            if (license === 'MIT'){
+                return  `\r[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`; 
+            } else if (license === 'GNU GPL v3.0') {
+                return `\r[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+            } else if (license === 'Apache 2.0') {
+                return `\r[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)` 
+            }
+        }
+    
+        const readMe = `
+        # ${data.title}
+        # ${getLicense}
+        # ${data.username} | ${res.data.email}
+        # ${res.data.avatar_url}
+    
+    
+        ## Table of Contents
+        1. Description
+        2. Installation
+        3. Usage
+        4. Contributors
+        5. Questions
+    
+    
+        # Description
+        ${data.description}
+        # Installation
+        ${data.installation}
+        # Usage
+        ${data.usage}
+        # Contributors
+        ${data.contributors}
+        # Questions
+        ${data.questions}`
       console.log(res.data);
+      fs.writeFile('README.md', readMe, (err) => {
+        if (err) {
+            throw err;
+        }
+    });
+    
     });
 
-    const getLicense = (license) => {
-        if (license === 'MIT'){
-            return  `\r[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`; 
-        } else if (license === 'GNU GPL v3.0') {
-            return `\r[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
-        } else if (license === 'Apache 2.0') {
-            return `\r[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)` 
-        }
-    }
-
-    const readMe = `
-    # ${data.title}
-    # ${getLicense}
-    # ${username} | ${res.data.email}
-    # ${res.data.avatar_url}
-
-
-    ## Table of Contents
-    1. Description
-    2. Installation
-    3. Usage
-    4. Contributors
-    5. Questions
-
-
-    # Description
-    ${data.description}
-    # Installation
-    ${data.installation}
-    # Usage
-    ${data.usage}
-    # Contributors
-    ${data.contributors}
-    # Questions
-    ${data.questions}`
-});
-
-fs.writeFile('README.md', readMe, (err) => {
-    if (err) {
-        throw err;
-    };
+   
 });
