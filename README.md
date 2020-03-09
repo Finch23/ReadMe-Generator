83744b7895e92f16abf6f3c1fa0cb6c3c07c71a0
Nicholas Finch <nicfinch223@gmail.com>
2020-03-09 12:57:56 -0400

    fixed image issue

--- README.md
+++ README.md
@@ -1,33 +1,25 @@
 
-        # ReadMe Generator
-        # (license) => {
-            if (license === 'MIT'){
-                return  `\r[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`; 
-            } else if (license === 'GNU GPL v3.0') {
-                return `\r[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
-            } else if (license === 'Apache 2.0') {
-                return `\r[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)` 
-            }
-        }
-        # finch23 | null
-        # https://avatars0.githubusercontent.com/u/57764795?v=4
-    
-    
-        ## Table of Contents
-        1. Description
-        2. Installation
-        3. Usage
-        4. Contributors
-        5. Questions
-    
-    
-        # Description
-        This project uses node modules to generate a read me file
-        # Installation
-        clone the repo and run npm install
-        # Usage
-        Use the terminal to answer the questions, and a read me file will be generated
-        # Contributors
-        Nicholas Finch
-        # Questions
-        N/A
+## My ReadMe Generator
+## undefined
+## finch23 | nicfinch223@gmail.com
+## ![img](https://avatars0.githubusercontent.com/u/57764795?v=4)
+
+
+## Table of Contents
+1. Description
+2. Installation
+3. Usage
+4. Contributors
+5. Questions
+
+
+# Description
+Uses inquirer and fs to generate a read me file
+# Installation
+1. Run npm install 2. Run npm init
+# Usage
+To generate a read me file based on users answers
+# Contributors
+Nicholas Finch
+# Questions
+N/A
--- index.js
+++ index.js
@@ -15,6 +15,11 @@ inquirer.prompt([
     },
     {
         type: 'input',
+        name: 'email',
+        message: 'What is your email?'
+    },
+    {
+        type: 'input',
         name: 'description',
         message: 'How would you describe this project?'
     },
@@ -54,42 +59,42 @@ inquirer.prompt([
     axios
     .get(`https://api.github.com/users/${data.username}`)
     .then(function(res) {
+        console.log(data.license)
         const getLicense = (license) => {
             if (license === 'MIT'){
                 return  `\r[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`; 
             } else if (license === 'GNU GPL v3.0') {
-                return `\r[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+                return `\r[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
             } else if (license === 'Apache 2.0') {
-                return `\r[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)` 
+                return `\r[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`; 
             }
         }
     
         const readMe = `
-        # ${data.title}
-        # ${getLicense}
-        # ${data.username} | ${res.data.email}
-        # ${res.data.avatar_url}
-    
-    
-        ## Table of Contents
-        1. Description
-        2. Installation
-        3. Usage
-        4. Contributors
-        5. Questions
-    
-    
-        # Description
-        ${data.description}
-        # Installation
-        ${data.installation}
-        # Usage
-        ${data.usage}
-        # Contributors
-        ${data.contributors}
-        # Questions
-        ${data.questions}`
-      console.log(res.data);
+## ${data.title}
+## ${getLicense(data.license)}
+## ${data.username} | ${data.email}
+## ![img](${res.data.avatar_url})
+
+
+## Table of Contents
+1. Description
+2. Installation
+3. Usage
+4. Contributors
+5. Questions
+
+
+# Description
+${data.description}
+# Installation
+${data.installation}
+# Usage
+${data.usage}
+# Contributors
+${data.contributors}
+# Questions
+${data.questions}`
       fs.writeFile('README.md', readMe, (err) => {
         if (err) {
             throw err;
